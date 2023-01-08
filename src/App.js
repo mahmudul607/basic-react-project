48e35c41feb004a7ba1f9edb235eeef501c2c187
mahmudul607 <111509406+mahmudul607@users.noreply.github.com>
2023-01-08 21:55:56 +0600

    product added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import banana from './products/banana.png';
+import apple from './products/apple.png';
+import pineapple from './products/pineapple.png';
+import mango from './products/mango.png';
 var style ={
   color: 'green',
   fontSize: '50px',
@@ -21,12 +25,20 @@ function App() {
         <p style={style}>Hello my dear {name}</p>
         <h2>I am {person1.firstName +" "+ person1.lastName}</h2>
         <p>My Reg No. is: {person1.Salary+bonous}</p>
-        <div style={{display:'flex'}}>
+        <div style={displayStyle}>
         <Person name={names[0]} job={jobs[0]}></Person>
         <Person name={names[1]} job={jobs[1]}></Person>
         <Person name={names[2]} job={jobs[2]}></Person>
         <Person name={names[3]} job={jobs[3]}></Person>
         </div>
+      <div style={displayStyle}>
+        <Product product={products[0]}></Product>
+        <Product product={products[1]}></Product>
+        <Product product={products[2]}></Product>
+        <Product product={products[3]}></Product>
+        
+      </div>
+       
       </header>
     </div>
   );
@@ -49,6 +61,37 @@ function Person(props){
       <img style={{width:'100%'}} src={logo} className="App-logo" alt="logo"/>
       <h1 >Name: {props.name}</h1>
       <h3>Department: {props.job}</h3>
+    </div>
+  )
+}
+const displayStyle = {
+  display:'flex',
+  margin:'5px'
+}
+const productImages =[apple, mango, banana, pineapple];
+const products = [
+  {Name:'Apple', Price:'$7.99', Conditions:'Fresh', image:productImages[0]},
+  {Name:'Mango', Price:'$5.99', Conditions:'Fresh', image:productImages[1]},
+  {Name:'Banana', Price:'$2.99', Conditions:'Fresh', image:productImages[2]},
+  {Name:'Pineapple', Price:'$6.99', Conditions:'Fresh', image:productImages[3]}
+]
+function Product(props){
+  const productStyle = {
+    border:'2px solid yellow',
+    backgroundColor:'gray',
+    height:'350px',
+    width:'300px',
+    padding:'10px',
+    margin:'5px'
+
+  }
+  const {Name , Price, image} = props.product;
+  return(
+    <div style={productStyle}>
+       <img style={{width:'40%', height:'40%'}} src={image} alt="" />
+      <h2 style={{color:'orange'}}>{Name}</h2>
+      <h4>Price: {Price}</h4>
+      <button>Buy Now</button>
     </div>
   )
 }
